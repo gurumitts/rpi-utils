2e443ce7df1e72e2b84f09be8869dcc87f0c0caf
gurumitts <miteshcoder1001@gmail.com>
2016-08-11 07:29:58 -0500

    fix RPI wifi

--- fix-wifi.sh
+++ fix-wifi.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#this script will disable WIFI power management to imporove overall wireless performance.  
+#This script will disable WIFI power management to imporove overall wireless performance.  
 
-#make sure the power management is off
+
+#Make sure the power management is off
 iwconfig wlan0 power off
 
 
 echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=0 rtw_ips_mode=1" >> /etc/modprobe.d/8192cu.conf
 
-#take a backup 
+#Take a backup 
 cp /etc/rc.local /etc/rc.local.original
 
-#this should be better:
-#only look for the last occurence of exit 0
-#however this should work well on a newly configured RPI with jessie
+
+#[TODO]Only look for the last occurrence of exit 0
+#However this should work well on a newly configured RPI with jessie
 sed -i 's/exit 0/#exit 0/g' /etc/rc.local
 
 echo " " >>/etc/rc.local
@@ -38,4 +39,4 @@ systemctl enable root-resume
 
 iwconfig
 
-echo "You should rebood your system now" 
+echo "You should reboot your system now" 
